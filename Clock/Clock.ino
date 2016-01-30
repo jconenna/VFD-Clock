b3b310d6a9fec2c4cf3e8f8f451c39bf314a1cd5
Joey Conenna <jconenna@knights.ucf.edu>
2016-01-30 18:50:59 -0500

    Arduino Code for VFD Clock with RTC

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,20 +1,16 @@
-// Arduino controlled Vacuum Flourescent Clock with RTC module
-// Last Modified: January 8th 2016
-// Author: Joey Conenna
-
-// Libraries for use with DS3232 RTC 
+// Libraries for DS3232 RTC 
 #include <DS3232RTC.h>  
 #include <Time.h>    
 #include <Wire.h>  
 
-#define SQW_PIN 2 // Pin to listen to the 1 HZ Square Wave from the RTC
-#define dt 1      // delay time
+#define SQW_PIN 2  
+#define dt 2      // delay time
 
-//Pin connected to ST_CP of both 74HC595
+//Pin connected to ST_CP of 74HC595
 #define LATCH  8
-//Pin connected to SH_CP of both 74HC595
+//Pin connected to SH_CP of 74HC595
 #define CLOCK 12
-////Pin connected to DS of Left 74HC595
+////Pin connected to DS of 74HC595
 #define DATA  11
 
 // Bytes to control VFD, 1 SEG OFF, 0 SEG ON. 
@@ -24,7 +20,7 @@ byte R = B11111111;      // x2a/b, a, b, c, d, e, f, g      // Right shift regis
 // Flag for RTC 1HZ SQ Wave ISR
 boolean interruptSet = false;
 
-// Vars to hold time data after fetching from RTC upon setup
+// Vars to hold time data after fetching from RTC on boot
 int seconds = 0;
 int minutes = 0;
 int hours = 0;
@@ -39,19 +35,19 @@ void setup()
 
   // Fetch time from RTC 
   tmElements_t tm;
+  
   RTC.read(tm);
   hours = tm.Hour;
   minutes = tm.Minute;
   seconds = tm.Second;
 
-  // Switch 24 HR to 12 HR format
+  // 24 HR -> 12 HR
   if(hours / 12 == 1)
     {
       AM = false;
       setPM();
       if(hours > 12)
         hours = hours % 12;
-      else hours = 12;
     }
   else
       setAM();
@@ -59,11 +55,11 @@ void setup()
       setTen();
   
     
-  // Set a 1 Hz Square Wave from the RTC
+  // Set 1 Hz SQ Wave from RTC connected to D2 pin
   RTC.squareWave(SQWAVE_NONE);
   RTC.squareWave(SQWAVE_1_HZ); 
   
-  // Set ISR on Pin 2 to interrupt each second with the Square Wave
+  // Set ISR on Pin 2 to interrupt each second
   pinMode(2, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), tick, FALLING);
 }
@@ -123,44 +119,38 @@ void loop()
   }
 
 
-  // Each loop iterration turns on and off each segment in a sequence to multiplex the VFD 
+  // Each loop iterration turns on and off each segment in sequence to multiplex the VFD \
   
-  // Turn on AM/PM, '1', and colon breifly
+  // Turn on AM/PM, '1', colon, and Hour 7 Segment briefly
+  setNumber(hours%10);
+  setSegment(2);
   setSegment(0);
   setSegment(1);
   setSegment(3);
   setDisplay();
   delay(dt);
+  clearSegment(2);
   clearSegment(0);
   clearSegment(1);
   clearSegment(3);
   
-  // Turn on hours 7 Segment briefly
-  setNumber(hours%10);
-  setSegment(2);
-  setDisplay();
-  delay(dt);
-  clearSegment(2);
-  
-  // Turn on first minutes 7 Segment briefly
+  // Turn on Tenths 7 Segment briefly
   setNumber(minutes/10);
   setSegment(4);
   setDisplay();
   delay(dt);
   clearSegment(4);
   
-  // Turn on second minutes 7 Segment briefly
+  // Turn on Last 7 Segment briefly
   setNumber(minutes%10);
   setSegment(5);
   setDisplay();
   delay(dt);
   clearSegment(5);
  
-} // loop
+}
 
 // Note for the following bitwise operations for the L and R bytes, that logical 1 = VFD segment OFF, and logical 0 = VFD segment ON
-// See schematic and note that a transistor is switched OFF when the output from the shift register is HIGH, which turns off a segment,
-// while a transistor is OFF when the shift register outputs LOW, which leaves a segment connected to 12 V rather than GND. 
 
 void setSegment(int seg) // seg 0-5
 {
@@ -312,13 +302,13 @@ void tick() // Interrupt Service Routine (ISR) is executed when the 1 HZ square-
 void setDisplay()  // Shifts out L and R bytes to shift registers and sets the states on for the VFD 
 {
   
-  // Set LATCH LOW to begin shifting out bytes to the shift register
+  // take the latchPin low so 
+  // the LEDs don't change while you're sending in bits:
   digitalWrite(LATCH, LOW);
-  
-  // Shift out the bytes
+  // shift out the byte:
   shiftOut(DATA, CLOCK, LSBFIRST, R);  
   shiftOut(DATA, CLOCK, LSBFIRST, L);
     
-  // Set LATCH HIGH to write the memory register to the output pins, turning on the segments
+  //take the latch pin high so the LEDs will light up:
   digitalWrite(LATCH, HIGH);
 }
